428208cacd6fddfe404a245fa78cf8d42b58f7a9
Nkoenyana <205151150+Nkoenyana@users.noreply.github.com>
2025-05-09 21:06:16 +0700

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,26 +5,12 @@ export const auth = defineAuth({
     email: true,
     externalProviders: {
       google: {
-        clientId: secret('GOOGLE_CLIENT_ID'),
-        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+        clientId: secret('679239044988-4i0htcjm6baa6kicpj6iqgn1jvlsr9m1.apps.googleusercontent.com'),
+        clientSecret: secret('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'),
                 
         attributeMapping: {
           email: 'email'
         }
-      },
-      signInWithApple: {
-        clientId: secret('SIWA_CLIENT_ID'),
-        keyId: secret('SIWA_KEY_ID'),
-        privateKey: secret('SIWA_PRIVATE_KEY'),
-        teamId: secret('SIWA_TEAM_ID')
-      },
-      loginWithAmazon: {
-        clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
-        clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET')
-      },
-      facebook: {
-        clientId: secret('FACEBOOK_CLIENT_ID'),
-        clientSecret: secret('FACEBOOK_CLIENT_SECRET')
       },
       callbackUrls: [
         'http://localhost:3000/profile',
