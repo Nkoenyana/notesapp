c48c2f4551c7731b3f07038ddf348b7fb7f716e1
yamisora-sys <20520930@ms.uit.edu.vn>
2025-05-31 18:12:01 +0700

    tmp

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -6,8 +6,19 @@ const schema = a.schema({
       name:a.string(),
       description: a.string(),
       image: a.string(),
+      createdAt: a.timestamp(),
+      updatedAt: a.timestamp(),
+      deletedAt: a.timestamp(),
     })
     .authorization((allow) => [allow.owner()]),
+    Tag: a.model({
+      name: a.string(),
+      description: a.string(),
+      color: a.string(),
+      author: a.string(),
+      createdAt: a.timestamp(),
+      updatedAt: a.timestamp(),
+    }).authorization((allow) => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
