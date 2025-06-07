6855be87dce92ad0aaeba7be12944015e6580b7b
yamisora-sys <20520930@ms.uit.edu.vn>
2025-06-07 17:05:42 +0700

    category, auth UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { signInWithRedirect } from "aws-amplify/auth";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import Homepage from "~pages/home.jsx";
 import { Routes, Route } from "react-router-dom";
 import NotFound from "~pages/code/404.jsx";
-signInWithRedirect({ provider: "Google" });
-import { client } from "~utils/amplify";
-/**
+import { CustomAuth, formFields  } from "~components/CustomAuth.jsx";
+/*
  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
  */
 
 
 export default function App() {
   return (
-      <Authenticator socialProviders={["Google"]}>
+      <Authenticator socialProviders={["Google"]}
+        components={CustomAuth}
+        formFields={formFields}
+      >
       {({ signOut }) => (
           <Routes>
             <Route
